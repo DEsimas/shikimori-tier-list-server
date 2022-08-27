21ad1c61f7321c270f94aa6901a49af634f80143
DEsimas <simasuh@gmail.com>
2022-08-28 02:29:12 +0300

    login verification

--- src/endpoints/post/login.js
+++ src/endpoints/post/login.js
@@ -6,8 +6,20 @@ export default async function login(req, res) {
     const { username, password } = req.body
 
     const userRecord = await prisma.User.findUnique({
-        where: { username }
+        where: { username },
+        select: {
+            id: true,
+            username: true,
+            auth: {
+                select: {
+                    password: true
+                }
+            }
+        }
     })
+
+    if (userRecord == null) res.status(400).send({ error: 'User does not exist' })
+    if (userRecord.auth.password != password) return res.status(400).send({ error: 'Wrong password' })
 
     const user = { username }
     res.json({ accessToken: generateAccessToken(user), refreshToken: await generateRefreshToken(user) })
