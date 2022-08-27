16d61e15bf98c6aeb98b1b0863ea2a08a18dcba4
DEsimas <simasuh@gmail.com>
2022-08-28 01:50:26 +0300

    get user in login

--- src/endpoints/post/login.js
+++ src/endpoints/post/login.js
@@ -1,8 +1,14 @@
 import generateAccessToken from '../../helpers/generateAccessToken.js'
 import generateRefreshToken from '../../helpers/generateRefreshToken.js'
+import prisma from '../../prisma.js'
 
 export default async function login(req, res) {
     const { username, password } = req.body
+
+    const userRecord = await prisma.User.findUnique({
+        where: { username }
+    })
+
     const user = { username }
     res.json({ accessToken: generateAccessToken(user), refreshToken: await generateRefreshToken(user) })
 }
