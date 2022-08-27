801a0cc28e8abf760f17e56c2541a81e3df10d7d
DEsimas <simasuh@gmail.com>
2022-08-28 00:34:57 +0300

    create refresh endpoing

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ dotenv.config()
 
 const app = express()
 
+const tokens = []
+
 app.use(express.json())
 
 app.get('/', authenticateToken, (req, res) => {
@@ -13,21 +15,39 @@ app.get('/', authenticateToken, (req, res) => {
     res.sendStatus(200)
 })
 
-app.post('/login', (req, res) => {
-    const {username, password} = req.body
-    const user = {username, password}
-    console.log(user)
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken })
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!tokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        res.json({ accessToken: generateAccessToken({ username: user.username }) })
+    })
 })
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body
+    const user = { username }
+    res.json({ accessToken: generateAccessToken(user), refreshToken: generateRefreshToken(user) })
+})
+
+function generateRefreshToken(user) {
+    const token = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '48h' })
+    tokens.push(token)
+    return token
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if(token == null) return res.sendStatus(401)
+    if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403)
+        if (err) return res.sendStatus(403)
         req.user = user
         next()
     })
