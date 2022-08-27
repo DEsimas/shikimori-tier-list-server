a3a956426580adaedf8c4d85a6501d027cf637ab
DEsimas <simasuh@gmail.com>
2022-08-28 00:45:10 +0300

    create middlewares dir

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { config } from 'dotenv'
 import express from 'express'
 import jwt from 'jsonwebtoken'
 
+import authenticateToken from './middlewares/authenticateToken.js'
+
 config()
 
 const app = express()
@@ -39,18 +41,6 @@ function generateRefreshToken(user) {
 
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
-}
-
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401)
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
 }
 
 app.listen(process.env.PORT)
